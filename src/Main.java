a96494257c1f1dccfd15556dd1f963cb216c9c6d
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-11-14 10:47:10 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        final String RESET = "\u001B[0m";
+        final String GREEN = "\u001B[32m";
+        final String YELLOW = "\u001B[33m";
+
+
+
+    }
+}
